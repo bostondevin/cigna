05e4d5724d659afab1f3c25f730dc1ee1f2cdf4c
Devin McQueeney <bostondevin@hotmail.com>
2017-11-05 09:02:17 -0500

    add data to tabs

--- src/app/core/shell/panel/panel.component.ts
+++ src/app/core/shell/panel/panel.component.ts
@@ -26,7 +26,16 @@ export class PanelComponent implements OnInit {
   ];
 
   c: any = {};
-  resolutions: any = [];
+  resolutions: any = []; //
+
+  templates: any = []; //
+  components: any = [];
+  collections: any = [];
+
+  componentGroups: any = []; //
+  collectionGroups: any = []; //
+  templateGroups: any = []; // <-- CREATE
+
 
 
   ngOnInit() {
@@ -35,6 +44,7 @@ export class PanelComponent implements OnInit {
     this.appService.get({service: 'constants'}).subscribe((c: any) => {
 
       this.c = c[0];
+
 
       this.appService.get({
         service: 'list', 
@@ -45,11 +55,110 @@ export class PanelComponent implements OnInit {
           this.c['Width'].sid,
           this.c['Height'].sid
         ]
-      }).subscribe((resolutions: any) => {
+      }).subscribe((d: any) => {
 
-        this.resolutions = resolutions;
+        this.resolutions = d;
 
       });
+
+
+
+
+
+
+
+      let cssPropsPopulate = [];
+      cssPropsPopulate.push(this.c['FontAwesomeIcon'].sid + ':' + this.c['ClassName'].sid);
+
+      this.appService.get({
+        service: 'list', 
+        id: this.c['TemplatesCollection'].sid,
+        select: [
+          this.c['Label'].sid,
+          this.c['FontAwesomeIcon'].sid
+        ],
+        populate: cssPropsPopulate.join(',')
+      }).subscribe((d: any) => {
+
+        this.templates = d;
+
+      });
+
+
+
+
+
+
+
+
+
+      this.appService.get({
+        service: 'list', 
+        id: this.c['NavigationGroupCollection'].sid,
+        select: [
+          '_sid',
+          this.c['Label'].sid,
+          this.c['FontAwesomeIcon'].sid
+        ],
+        populate: cssPropsPopulate.join(',')
+      }).subscribe((collectionGroups: any) => {
+
+        this.collectionGroups = collectionGroups;
+
+        this.appService.get({
+          service: 'list', 
+          id: this.c['Thing'].sid,
+          select: [
+            '_sid',
+            this.c['Label'].sid,
+            this.c['FontAwesomeIcon'].sid,
+            this.c['UserGroups'].sid,
+            this.c['NavigationGroup'].sid
+          ]
+        }).subscribe((collections: any) => {
+
+          this.collections = collections;
+
+          for (let group of collectionGroups) {
+
+            let count = 0;
+
+            for (let collection of collections) {
+              if (collection[this.c['Label'].sid] === group._id)
+                count = count + 1;
+            }
+
+            if (count > 0)
+               this.collectionGroups.push(group);
+
+          }
+
+        });
+
+      });
+
+
+
+
+
+      this.appService.get({
+        service: 'list', 
+        id: this.c['ComponentGroupCollection'].sid,
+        select: [
+          this.c['Label'].sid,
+          this.c['FontAwesomeIcon'].sid
+        ]
+      }).subscribe((d: any) => {
+
+        this.componentGroups = d;
+
+      });
+
+
+
+
+
+
     });
 
 
@@ -85,6 +194,36 @@ export class PanelComponent implements OnInit {
 
   }
 
+
+
+
+  addNewPage(button: any){
+
+    alert('Added');
+/*
+    if ($scope.data.newPageName){
+
+        var o = {};
+        o[c.Label] = $scope.data.newPageName || 'New Page';
+        o[c.ThingType]= c.TemplatesCollection_id;
+        o[c.ApplicationsProperty] = [_appId];
+        o[c.JsonData] = {Default: []};
+
+        AppService.save(o).then(function(data){
+            $scope.data.newPageName = null;
+            AppService.setSetting('page', data);
+            $scope.page = AppService.getSetting('page')._sid;
+            $scope.getLayouts(true);
+            $state.go($state.current, {page: AppService.getSetting('page')._sid, id: null, isFresh: null});
+        });
+
+    } else {
+        toastr.error('Please enter a title for the new page');
+        focus('newPageTitle');
+    }
+*/
+
+  }
 
   setResolution(resolution: any){
     console.log('Width ' + resolution[this.c['Width'].sid]);
